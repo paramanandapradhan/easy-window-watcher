a72b43f51f73319de32c3f150f451b0138e63db2
Param <param@mac.com>
2024-04-09 16:03:45 +0530

    Get value api added

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -11,49 +11,11 @@ class WindowWatcher {
         this.key = windowKey;
     }
 
-    private checkKey(obj: any, path: string) {
-        // Split the path into an array of keys
-        const keys = path.split('.');
-        // Start with the initial object
-        let current = obj;
 
-        // Traverse the object using the keys
-        for (const key of keys) {
-            if (current[key] === undefined) {
-                // If the key does not exist, return false
-                return false;
-            }
-            // Move to the next level in the object
-            current = current[key];
-        }
-
-        // If all keys are found, return true
-        return true;
-    }
-
-    private getValue(obj: any, path: string) {
-        // Split the path into an array of keys
-        const keys = path.split('.');
-        // Start with the initial object
-        let current = obj;
-
-        // Traverse the object using the keys
-        for (const key of keys) {
-            if (current[key] === undefined) {
-                // If the key does not exist, return false
-                return undefined;
-            }
-            // Move to the next level in the object
-            current = current[key];
-        }
-
-        // If all keys are found, return true
-        return current;
-    }
 
     private check(resolve: any) {
-        if (this.checkKey(window, this.key)) {
-            resolve(this.getValue(window, this.key))
+        if (getValue(window, this.key)) {
+            resolve(getValue(window, this.key))
         } else {
             this.waitCount += this.interval;
 
@@ -74,6 +36,26 @@ class WindowWatcher {
             this.check(resolve);
         })
     }
+}
+
+export function getValue(obj: any, path: string) {
+    // Split the path into an array of keys
+    const keys = path.split('.');
+    // Start with the initial object
+    let current = obj;
+
+    // Traverse the object using the keys
+    for (const key of keys) {
+        if (current[key] === undefined) {
+            // If the key does not exist, return false
+            return undefined;
+        }
+        // Move to the next level in the object
+        current = current[key];
+    }
+
+    // If all keys are found, return true
+    return current;
 }
 
 async function watchWindowValue<T>(key: string) {
