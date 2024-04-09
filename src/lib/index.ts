f3a99b6f2cda303a9a22592d2585d4e094189343
Param <param@mac.com>
2024-04-09 15:27:07 +0530

    Fixed issue

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -27,9 +27,29 @@ class WindowWatcher {
         return true;
     }
 
+    private getValue(obj: any, path: string) {
+        // Split the path into an array of keys
+        const keys = path.split('.');
+        // Start with the initial object
+        let current = obj;
+
+        // Traverse the object using the keys
+        for (const key of keys) {
+            if (current[key] === undefined) {
+                // If the key does not exist, return false
+                return undefined;
+            }
+            // Move to the next level in the object
+            current = current[key];
+        }
+
+        // If all keys are found, return true
+        return current;
+    }
+
     private check(resolve: any) {
         if (this.checkKey(window, this.key)) {
-            resolve(window[this.key])
+            resolve(this.getValue(window, this.key))
         } else {
             setTimeout(() => {
                 this.check(resolve);
